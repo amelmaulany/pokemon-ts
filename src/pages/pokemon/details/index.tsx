93629c495f9bb27c643439775d5672540ba13ad1
Akbar Rizqi Luhur Pambudi <akbar.pambudi@amarbank.co.id>
2023-06-05 11:43:25 +0700

    fixing error and resposive on pokemon and pokemon details page

--- src/pages/pokemon/details/index.tsx
+++ src/pages/pokemon/details/index.tsx
@@ -31,7 +31,7 @@ export default function PokemonDetailsPage() {
     }, [state.encounters]);
 
     useEffect(() => {
-        
+
     })
 
     return (
@@ -39,105 +39,108 @@ export default function PokemonDetailsPage() {
             <div className="px-16 py-5 flex flex-col gap-6">
                 <div className="flex flex-row justify-start items-center gap-10">
                     {
-                        state.loadingDetails  || !pokemon ?
-                        <p>Loading...</p> :
-                        <div className="rounded-md w-full p-12" style={{ backgroundColor: pokemon.types ? getBgTypesColor(pokemon.types[0].type.name) : '#c1c1c1' }}>
-                            <div className="flex flex-row items-center gap-9">
-                                <img
-                                    src={pokemon.sprites.other["official-artwork"].front_default}
-                                    alt={pokemon.name}
-                                    className="rounded-lg w-1/3"
-                                />
-                                <div className="flex flex-col justify-start gap-3 w-2/3">
-                                    {/* name */}
-                                    <div className="flex flex-row items-center gap-5">
-                                        <p className="text-4xl font-semibold">
-                                            {pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}
+                        state.loadingDetails || !pokemon ?
+                            <p>Loading...</p> :
+                            <div className="rounded-md w-full p-12" style={{ backgroundColor: pokemon.types ? getBgTypesColor(pokemon.types[0].type.name) : '#c1c1c1' }}>
+                                <div className="flex sm:flex-col md:flex-row items-center gap-9 mb-7">
+                                    {/* image */}
+                                    <img
+                                        src={pokemon.sprites.other["official-artwork"].front_default}
+                                        alt={pokemon.name}
+                                        className="rounded-lg w-1/3"
+                                    />
+                                    <div className="flex flex-col justify-start gap-3 w-2/3">
+                                        {/* name */}
+                                        <div className="flex flex-row items-center gap-5">
+                                            <p className="text-4xl font-semibold">
+                                                {pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}
+                                            </p>
+                                            {pokemon.is_default && <div className="rounded-lg bg-orange-300 p-2"><b>default pokemon</b></div>}
+                                        </div>
+                                        {/* order */}
+                                        <p className="text-2xl font-semibold text-gray-700 text-opacity-60">
+                                            #00{pokemon.order}
                                         </p>
-                                        {pokemon.is_default && <div className="rounded-lg bg-orange-300 p-2"><b>default pokemon</b></div>}
-                                    </div>
-                                    {/* order */}
-                                    <p className="text-2xl font-semibold text-gray-700 text-opacity-60">
-                                        #00{pokemon.order}
-                                    </p>
 
-                                    <div className="flex flex-row gap-4">
-                                        {/* types */}
-                                        <div className="flex flex-row justify-start items-center gap-2 bg-slate-100 p-5 rounded-md w-full">
-                                                <p className="text-lg"><b>Types&nbsp;&nbsp;</b></p> 
+                                        <div className="flex lg:flex-row md:flex-col sm:flex-col sm:w-full gap-4">
+                                            {/* types */}
+                                            <div className="flex flex-row justify-start items-center gap-2 bg-slate-100 p-5 rounded-md md:w-full">
+                                                <p className="text-lg"><b>Types&nbsp;&nbsp;</b></p>
                                                 <div className="flex flex-row gap-2">
                                                     {pokemon.types.map(
-                                                        (type: Type) => 
-                                                        <div className="p-3 w-auto rounded-md flex flex-row justify-start items-center gap-3 shadow-md bg-slate-300">
-                                                            <p>{type.type.name}</p> <div className="h-4 w-4 rounded-full" style={{ backgroundColor: type ? getBgTypesColor(type.type.name) : '#c1c1c1' }}>&nbsp;</div>
-                                                        </div>
+                                                        (type: Type) =>
+                                                            <div className="p-3 w-auto rounded-md flex flex-row justify-start items-center gap-3 shadow-md bg-slate-300">
+                                                                <p>{type.type.name}</p> <div className="h-4 w-4 rounded-full" style={{ backgroundColor: type ? getBgTypesColor(type.type.name) : '#c1c1c1' }}>&nbsp;</div>
+                                                            </div>
                                                     )}
                                                 </div>
+                                            </div>
+
+                                            {/* weight */}
+                                            <div className="flex flex-row gap-4">
+                                                <div className="flex flex-row justify-start items-center gap-2 bg-slate-100 p-5 rounded-md w-full">
+                                                    <p className="text-lg"><b>Weight:&nbsp;&nbsp;</b></p>
+                                                    <p>{pokemon.weight / 10}&nbsp;kg</p>
+                                                </div>
+                                            </div>
+
+                                            {/* height */}
+                                            <div className="flex flex-row gap-4">
+                                                <div className="flex flex-row justify-start items-center gap-2 bg-slate-100 p-5 rounded-md w-full">
+                                                    <p className="text-lg"><b>Height:&nbsp;&nbsp;</b></p>
+                                                    <p>{pokemon.height / 10}&nbsp;m</p>
+                                                </div>
+                                            </div>
                                         </div>
 
-                                        {/* weight */}
+                                        {/* Location / Area */}
                                         <div className="flex flex-row gap-4">
                                             <div className="flex flex-row justify-start items-center gap-2 bg-slate-100 p-5 rounded-md w-full">
-                                                <p className="text-lg"><b>Weight:&nbsp;&nbsp;</b></p>
-                                                <p>{pokemon.weight/10}&nbsp;kg</p>
+                                                <p className="text-lg"><b>Location Area Encounter:&nbsp;&nbsp;</b></p>
+                                                <div className="flex flex-row gap-2">
+                                                    {state.locations.length > 0 ? state.locations.map(
+                                                        (location: LocationArea) =>
+                                                            <div className="p-3 w-auto rounded-md flex flex-row justify-start items-center gap-3 shadow-md bg-slate-300">
+                                                                <div className="rounded-full bg-slate-300">{location.names[0].name}</div>
+                                                            </div>
+                                                    ) : 'unknown'}
+                                                </div>
                                             </div>
                                         </div>
 
-                                        {/* height */}
-                                        <div className="flex flex-row gap-4">
-                                            <div className="flex flex-row justify-start items-center gap-2 bg-slate-100 p-5 rounded-md w-full">
-                                                <p className="text-lg"><b>Height:&nbsp;&nbsp;</b></p>
-                                                <p>{pokemon.height/10}&nbsp;m</p>
-                                            </div>
-                                        </div>
                                     </div>
-                                    
-                                    {/* Location / Area */}
-                                    <div className="flex flex-row gap-4">
-                                        <div className="flex flex-row justify-start items-center gap-2 bg-slate-100 p-5 rounded-md w-full">
-                                            <p className="text-lg"><b>Location Area Encounter:&nbsp;&nbsp;</b></p>
-                                            <div className="flex flex-row gap-2">
-                                                {state.locations.length > 0 ? state.locations.map(
-                                                    (location: LocationArea) => 
-                                                        <div className="p-3 w-auto rounded-md flex flex-row justify-start items-center gap-3 shadow-md bg-slate-300">
-                                                            <div className="rounded-full bg-slate-300">{location.names[0].name}</div>
-                                                        </div>
-                                                ) : 'unknown'}
-                                            </div>
-                                        </div>
-                                    </div>
-                                    
                                 </div>
-                            </div>
-                            
-                            {/* stats */}
-                            <div className="flex flex-col gap-3 bg-slate-100 p-6 rounded-md w-max">
-                                <p className="text-lg"><b>Base Stats:</b></p>
-                                <div className="flex flex-col justify-start items-start gap-1">
-                                    {pokemon.stats.map((stat: Stat) => 
-                                        <div className="flex flex-col justify-start items-start gap-2 p-3 rounded-md">
-                                            <b>{stat.stat.name.charAt(0).toUpperCase() + stat.stat.name.slice(1)}: {stat.base_stat}</b>
-                                            <div className="w-60 bg-slate-300 shadow-lg rounded-full h-2.5 dark:bg-gray-700">
-                                                <div className="h-2.5 rounded-full" style={{ width: stat.base_stat, backgroundColor: pokemon.types ? getBgTypesDarkColor(pokemon.types[0].type.name) : '#000'}}></div>
+
+                                {/* stats */}
+                                <div className="flex flex-col gap-3 bg-slate-100 p-6 rounded-md w-max">
+                                    <p className="text-lg"><b>Base Stats:</b></p>
+                                    <div className="flex flex-col justify-start items-start gap-1">
+                                        {pokemon.stats.map((stat: Stat) =>
+                                            <div className="flex flex-col justify-start items-start gap-2 p-3 rounded-md">
+                                                <b>{stat.stat.name.charAt(0).toUpperCase() + stat.stat.name.slice(1)}: {stat.base_stat}</b>
+                                                <div className="w-60 bg-slate-300 shadow-lg rounded-full h-2.5 dark:bg-gray-700">
+                                                    <div className="h-2.5 rounded-full" style={{ width: stat.base_stat, backgroundColor: pokemon.types ? getBgTypesDarkColor(pokemon.types[0].type.name) : '#000' }}></div>
+                                                </div>
                                             </div>
-                                        </div>
-                                    )}
-                                </div>
-                            </div>
-
-                            {/* evolution */}
-                            {evolution 
-                                && evolution.length > 0 
-                                &&  <div className="flex flex-row gap-3 bg-slate-100 p-6 rounded-md w-max">
-                                        <div className="rounded-full">
-                                            <img src={evolution.species} />
-                                        </div>
+                                        )}
                                     </div>
-                            }
-                            
+                                </div>
+
+                                {/* evolution */}
+                                {
+                                    // evolution
+                                    //     && evolution.length > 0
+                                    //     && <div className="flex flex-row gap-3 bg-slate-100 p-6 rounded-md w-max">
+                                    //         <div className="rounded-full">
+                                    //             <img src={evolution.species} />
+                                    //         </div>
+                                    //     </div>
+                                    <div>evolution</div>
+                                }
 
 
-                        </div>
+
+                            </div>
                     }
                 </div>
             </div>
--- src/pages/pokemon/index.tsx
+++ src/pages/pokemon/index.tsx
@@ -7,6 +7,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { PokemonProps, pokemonAction } from '../../store/slice/pokemon';
 import { Pokemon } from '../../models/pokemon';
 import { useNavigate } from 'react-router-dom';
+import NoImage from '../../assets/no-photos.png';
 
 export default function PokemonPage() {
     const dispatch = useDispatch();
@@ -25,23 +26,24 @@ export default function PokemonPage() {
 
     return (
         <Layout>
-            <div className="px-16 py-5 grid grid-cols-3 gap-6">
-                {!pokemonData.loading && !pokemonData.loadingDetails && pokemonData.pokemonDetails.length > 0
+            <div className="px-16 py-5 grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
+                {!pokemonData.loading && !pokemonData.loadingDetails && pokemonData.pokemonDetails && pokemonData.pokemonDetails.length > 0
                     ? pokemons.map(
-                        (pokemon: Pokemon, i: number) => 
+                        (pokemon: Pokemon, i: number) =>
                             <div onClick={() => navigate(`/pokemon/${pokemonData.pokemonDetails[i].id}`)}>
                                 <PokemonBox
-                                  name={pokemon.name}
-                                  loading={pokemonData.loadingDetails}
-                                  image={pokemonData.pokemonDetails[i].sprites.front_default}
-                                  loadingDetails={pokemonData.loadingDetails}
-                                  type={pokemonData.pokemonDetails[i].types[0].type.name}
-                                />                                
+                                    key={i}
+                                    name={pokemon.name}
+                                    loading={pokemonData.loadingDetails}
+                                    image={pokemonData.pokemonDetails[i] ? pokemonData.pokemonDetails[i].sprites.front_default : NoImage}
+                                    loadingDetails={pokemonData.loadingDetails}
+                                    type={pokemonData.pokemonDetails[i] ? pokemonData.pokemonDetails[i].types[0].type.name : 'type loading..'}
+                                />
                             </div>
 
-                        )
+                    )
                     :
-                    [1,2,3,4,5,6,7,8,9,10].map((item) => <PokemonBox loading={true} loadingDetails={true} />)}   
+                    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((item) => <PokemonBox loading={true} loadingDetails={true} />)}
             </div>
         </Layout>
     );
